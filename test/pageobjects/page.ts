aacddb95c4bb3a5d062cd0c0490725cc53747124
mparimi06 <mparimi06@users.noreply.github.com>
2021-08-10 10:25:08 +0100

    Update page.ts

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -3,10 +3,6 @@
 * that is shared across all page objects
 */
 export default class Page {
-   
-    // open (path: string) {
-    //     return browser.url(`https://the-internet.herokuapp.com/${path}`)
-    // }
 
     open (url: string) {
         return browser.url(url)
