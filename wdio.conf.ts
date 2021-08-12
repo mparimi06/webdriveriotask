c262746f6479efb24ff509aefad302b02e181c53
mparimi06 <mparimi06@users.noreply.github.com>
2021-08-12 16:07:10 +0100

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -49,7 +49,7 @@ export const config: WebdriverIO.Config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [
+   capabilities: [
     
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
@@ -57,12 +57,7 @@ export const config: WebdriverIO.Config = {
          {
             browserName: 'chrome',
           },
-          {
-            browserName: 'firefox',
-            'moz:firefoxOptions': {
-                binary: 'C:\Users\mparimi\AppData\Local\Mozilla Firefox'
-            }
-          },
+        
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
